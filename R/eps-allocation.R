353dd456b404a6a791276e1e81c4fc82f8ea6eef
J. Livsey <james.a.livsey@gmail.com>
2020-03-06 09:31:08 -0500

    initial eps allocation funcs

---
+++ R/eps-allocation.R
@@ -0,0 +1,44 @@
+#' Check Geography associated with given margin
+#'
+#' @param mar list of length 6 with the 6th being Geo margin
+#'
+#' @return Geo level in ('state', 'county', 'tract')
+#' @export
+#'
+geo_of_mar <- function(mar){
+  if(length(mar[[6]]) > 6){
+    return("state")
+  } else if(length(mar[[6]]) > 1 ){
+    return("county")
+  } else if(length(mar[[6]] == 1)){
+    return("tract")
+  } else{
+    stop("Geo of margin is not an expected length")
+  }
+}
+
+
+#' Check query associated with a given margin
+#'
+#' @param mar list of length 6 with first 1-5 elements being margins
+#'            associated with hhgq, sex, cenrace, age, hisp in that order
+#'
+#' @return
+#' @export
+#'
+query_of_mar <- function(mar){
+  zerosMar <- lapply(mar, function(x){x==0})
+  zerosMar <- unlist(zerosMar)
+  if(zerosMar[2] == 0 &&
+     zerosMar[3] == 0 &&
+     zerosMar[4] == 0 &&
+     zerosMar[5] == 0 ){
+    return("hhgq")
+  }else if(zerosMar[1] == 0 && zerosMar[2] == 0){
+    return("votingAge_hisp_cenrace")
+  }else if(zerosMar[1] == 0 && zerosMar[3] == 0 && zerosMar[5] == 0){
+    return("age_sex")
+  }else{
+    return("detail")
+  }
+}
