dea68ef52d28ee778136013aebfd1b83fc24a49c
James A Livsey (CENSUS/CSRM FED) <james.a.livsey@census.gov>
2020-03-10 18:32:42 -0400

    improve memory usage

--- tests/single-L1fit-sim6way.R
+++ tests/single-L1fit-sim6way.R
@@ -3,6 +3,10 @@
 
 library(L1pack)
 library(Dvar)
+library(tictoc)
+
+# load marPack object
+source('~/GitHub/Dvar/tests/setup-marPack.R')
 
 options(warn = -1)
 
@@ -21,7 +25,7 @@ A <- array(sample(1:10, size = prod(mydim), replace = TRUE), mydim)
   W <- NULL
 
   # New parameters
-  geoMod <- c(.5, .25, .25)
+  geoMod <- c(.2, .5, .3)
   queryMod <- c(.1, .2, .5, .2)
 
 
@@ -29,23 +33,29 @@ A <- array(sample(1:10, size = prod(mydim), replace = TRUE), mydim)
   # dimension of 'middle' of table. No margins
   dim0 = dim(intab)
   d <- prod(dim0) # total number of unknowns
+  # number of total covariate that go in model (including specified margins)
+  ndat = length(marPack) + d
 
   # vectorize true values at indices that go into model (will add noise)
-  y_true = c(intab)
+  y_true <- rep(-99, ndat)
+  y_true[1:d] <- c(intab)
 
   # Initalize Design matrix for model at Identity = will be using all scalar
   #   elements of table for model.
   #   Will rbind margins within function
-  X <- diag(d)
+  X <- matrix(-99, ndat, d)
+  X[1:d, 1:d] <- diag(d)
 
   # Initialize epsilon modifier
-  epsMod <- geoMod[3] * # tract
-            queryMod[1] # detailed
-  epsMod <- rep(epsMod, d)
+  epsMod_init <- geoMod[3] * # tract
+                 queryMod[1] # detailed
+  epsMod <- rep(-99, ndat)
+  epsMod[1:d] <- epsMod_init
 
   # If no weight matrix is passed initialize to all ones
   if(is.null(W)) W <- rep(1, d + length(marPack))
 
+tic()
   # Loop over marpack and add corresponding row to X and value to y
   for(i in 1:length(marPack)){
     if(i < 10) print(i)
@@ -59,21 +69,18 @@ A <- array(sample(1:10, size = prod(mydim), replace = TRUE), mydim)
     # Find next row for design matrix
     Zv <- c(arraySetOnes(dim0, mar))
     # Add to design matrix
-    X <- rbind(X, Zv)
+    X[d+i, ] <- Zv
     # Find next row for y
     newY <- sum(arrayEval(intab, mar))
     # Append newY to y_true vector
-    y_true <- c(y_true, newY)
+    y_true[d+i] <- newY
     # Append epsilon modifier to match with y_true and specified margin
     epsMod_new <- geoChar2geoMod(geoMod, g) *
                   queryChar2queryMod(queryMod, q)
-    epsMod <- c(epsMod, epsMod_new)
-    # Sanity check - remove for large problems after working
-    if( (intab %*% Zv) != newY ) warning("new y value not equal to table x X")
+    epsMod[d+i] <- epsMod_new
   }
+toc() # Time to run 287 seconds = 4.78 mins
 
-  # number of total covariate that go in model (including specified margins)
-  ndat = length(marPack) + prod(dim0)
   # SANITY CHECK
   if( ndat != length(y_true) ) warning("ndat != length(y_true)")
 
@@ -90,7 +97,7 @@ A <- array(sample(1:10, size = prod(mydim), replace = TRUE), mydim)
     # Fit model and return estimated coefs
 tic()
         coefEsts = l1fit(W*X, W*y, int=F)$coef
-toc()
+toc() # timer: 1070 seconds = 17.8 mins
   # }
 
   # Take mean of all param estimates (will be output)
