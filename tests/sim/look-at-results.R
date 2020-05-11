b616a689688b6cc122e4fb961a979e1bcfafca10
James A Livsey (CENSUS/CSRM FED) <james.a.livsey@census.gov>
2020-05-11 11:37:56 -0400

    res investigation

---
+++ tests/sim/look-at-results.R
@@ -0,0 +1,31 @@
+# Take mean of all param estimates (will be output)
+mu = apply(coefEsts, 2, mean)
+
+# mean square error (will be output)
+rmse.cell = sqrt(apply(coefEsts^2,2,mean)-mu^2)
+
+# histogram of of RMSE's
+my_rmse <- rmse[rmse < 2]
+hist(my_rmse, breaks = 30)
+
+
+my_hist <- function(x, thresh = 10){
+  xx <- x[x<thresh]
+  hist(xx, breaks = 30, xlim = c(0, 2))
+}
+
+my_hist(rmse.all, 2)
+my_hist(rmse.none, 2)
+my_hist(rmse.cell, 2)
+
+
+
+# Cell analysis
+
+cellEst.all <- coefEsts[1:100, 1]
+cellEst.none <- coefEsts[, 1]
+cellEst.cell <- coefEsts[, 1]
+
+hist(cellEst.all, xlim = c(-2, 6), breaks = 30)
+hist(cellEst.none, xlim = c(-2, 6), breaks = 30)
+hist(cellEst.cell, xlim = c(-2, 6), breaks = 30)
