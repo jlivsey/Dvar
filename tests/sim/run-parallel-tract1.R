bd6a371c193de3556de92b5e89eadfa9b9286ab1
J. Livsey <james.a.livsey@gmail.com>
2020-06-05 10:59:23 -0400

    run for tract1 and tract1-county1

---
+++ tests/sim/run-parallel-tract1.R
@@ -0,0 +1,43 @@
+library(Dvar)
+library(L1pack)
+library(parallel)
+
+# load marPack object
+ source('~/GitHub/Dvar/tests/sim/setup-marPack-tract1.R')
+
+# privacy budget parameters
+bpar <- 4
+geoMod <- c(.2, .5, .3)
+queryMod <- c(.1, .2, .5, .2)
+
+#' True table
+load("~/github/Dvar/tests/sim/true-table.RData")
+
+# make function that takes dummy arguement to pass to mclapply( )
+run_sim6way <- function(trial){
+  Sim6Way(Nrep = 1,
+          intab = A,
+          bpar = bpar,
+          marPack = marPack,
+          geoMod = geoMod,
+          queryMod = queryMod)
+}
+
+# Number of cores
+numCores <- detectCores()
+
+# Number of replicates
+trials <- 1:100
+
+# Run in parallel
+library(tictoc)
+tic()
+coefEsts_tract1 <- mclapply(trials, run_sim6way, mc.cores = numCores)
+toc()
+
+# convert list object to matrix
+a <- matrix(unlist(coefEsts_tract1), ncol = 3360, byrow = TRUE)
+coefEsts_tract1 <- a
+
+# save to directory
+save(coefEsts_tract1, file = "~/GitHub/Dvar/tests/sim/sim-results-2020-06-04-tract1.RData")
---
+++ tests/sim/run-tract1.R
@@ -0,0 +1,26 @@
+library(Dvar)
+library(L1pack)
+
+# load marPack object
+source('~/GitHub/Dvar/tests/sim/setup-marPack-tract1.R')
+
+# privacy budget parameters
+bpar <- 4
+geoMod <- c(.2, .5, .3)
+queryMod <- c(.1, .2, .5, .2)
+
+#' True table
+# mydim = c(2, 2, 7, 3, 2, 20)
+# set.seed(123)
+# A <- array(sample(0:10, size = prod(mydim), replace = TRUE), mydim)
+load("~/github/Dvar/tests/sim/true-table.RData")
+
+OUT_tract1 <-
+  Sim6Way(Nrep = 300,
+          intab = A,
+          bpar = bpar,
+          marPack = marPack,
+          geoMod = geoMod,
+          queryMod = queryMod)
+
+# save(OUT, file = "~/GitHub/Dvar/tests/sim/sim-results-2020-03-16.RData")
