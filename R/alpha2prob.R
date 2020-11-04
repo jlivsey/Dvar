103ad1cfd338fcbb8dd62a830031102e31c7dfea
J. Livsey <james.a.livsey@gmail.com>
2020-11-04 07:08:30 -0500

    upd

--- R/alpha2prob.R
+++ R/alpha2prob.R
@@ -1,32 +1,30 @@
-I <- c(2, 3, 4)
-J
-Jcheck(J)
-lenJ <- sapply(J, length)
-P <- array(dim = I)
+#' Convert valid alpha array to probabilities under log-linear formulation
+#'
+#' @param A alpha array. Must be valid and provide identifiable log-linear
+#' specification
+#' @param J index List of variable interactions to include
+#' @param I vector. Number of categories for each variable in sence
+#'
+#' @return an array of dimension I with probabilities
+#' @export
+#'
 
-# all indicies of P array (by construction all values are NA)
-Pidx <- which(is.na(P), arr.ind = TRUE)
 
-for(i in 1:nrow(Pidx)){
-
-  x <- Pidx[i, ]
-
-  Px <- 0
-
-  for(k in 1:max(lenJ)) {  # loop over different length of J
-
-    for(j in which(lenJ == k)){
-
-      Px <- Px + A[[j]][matrix(x[J[[j]]], nrow = 1)]
-
+alpha2prob <- function(A, J, I){
+  Jcheck(J)
+  lenJ <- sapply(J, length)
+  P <- array(dim = I)
+  # all indicies of P array (by construction all values are NA)
+  Pidx <- which(is.na(P), arr.ind = TRUE)
+  for(i in 1:nrow(Pidx)){
+    x <- Pidx[i, ]
+    Px <- 0
+    for(k in 1:max(lenJ)) {  # loop over different length of J
+      for(j in which(lenJ == k)){
+        Px <- Px + A[[j]][matrix(x[J[[j]]], nrow = 1)]
+      }
     }
-
-
+    P[matrix(x, nrow = 1)] <- exp(Px) / (1 + exp(Px))
   }
+} # end alpha2prob()
 
-  P[matrix(x, nrow = 1)] <- exp(Px) / (1 + exp(Px))
-
-}
-
-p <- seq(0, 1, .0001)
-plot(log(p)~p)
--- R/check_valid.R
+++ R/check_valid.R
@@ -1,17 +1,3 @@
-# Just to get started make an array J of all touples that will be included
-J <- list()
-J[[1]] <- 1
-J[[2]] <- 2
-J[[3]] <- 3
-J[[4]] <- 4
-J[[5]] <- 5
-J[[6]] <- 6
-J[[7]] <- c(2, 4)
-J[[8]] <- c(2, 6)
-J[[9]] <- c(3, 6)
-J[[10]]<- c(1, 2, 3)
-
-
 # function to calculate all subtouples
 all_subtouples <- function(v){
   A <- list()
