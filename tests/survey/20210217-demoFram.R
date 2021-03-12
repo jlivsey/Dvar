037657b163542d5f522a87161fe2ebb54405f2c2
J. Livsey <james.a.livsey@gmail.com>
2021-03-12 09:26:52 -0500

    Update 20210217-demoFram.R

--- tests/survey/20210217-demoFram.R
+++ tests/survey/20210217-demoFram.R
@@ -1,7 +1,31 @@
+A <- array(0, dim = c(2, 2, 4, 7))
+dimnames(A)[[1]] <- c("mal", "fem")
+dimnames(A)[[2]] <- c("own", "rnt")
+dimnames(A)[[3]] <- c(letters[1:4])
+dimnames(A)[[4]] <- c(LETTERS[1:7])
+
+(n  = 2*2*4*7)
+(N  = 20*n)
+int = rep(1, n)
+
+sex = factor(rep(0:1, n/2), labels = c("mal", "fem"))
+own = factor(rep(0:1, n/2), labels = c("own" , "rnt"))
+rac = factor(rep(0:3, n/4), labels = c("wht", "blc", "pac", "oth"))
+geo = factor(rep(0:6, n/7), labels = c("ge1", "ge2", "ge3", "ge4", "ge5", "ge6", "ge7"))
+sxg = interaction(sex, geo)
+
+sex = factor(rep(0:1, n/2), labels = c("male", "female"))
+own = factor(rep(0:1, n/2), labels = c("own" , "rent"))
+rac = factor(rep(0:3, n/4))
+geo = factor(rep(0:6, n/7))
+sxg = interaction(sex, geo)
 
 dfA <- melt(A)
+colnames(dfA) <- c("sex", "own", "rac", "geo", "value")
+head(dfA)
 
-demoFram = matrix(nrow = nrow(dfA), ncol = 24)
+
+demoFram = matrix(nrow = nrow(dfA), ncol = 1+1+3+6+13)
 
 for(i in 1:nrow(dfA)){
 
@@ -17,17 +41,21 @@ for(i in 1:nrow(dfA)){
                       contrasts(own)[v[2], ],
                       contrasts(rac)[v[3], ],
                       contrasts(geo)[v[4], ],
-                      contrasts(sexgeo)[sexgeoChar, ]
+                      contrasts(sxg)[sexgeoChar, ]
                     )
 
 }
 
+# add intercept column to demoFram
+demoFram = cbind(int, demoFram)
+
+head(demoFram)
 
 # Are all rows unique?
-temp <-  data.frame(demoFram) %>%
-         group_by_all() %>%
-         summarise(COUNT = n())
-dim(temp)
+# temp <-  data.frame(demoFram) %>%
+#          group_by_all() %>%
+#          summarise(COUNT = n())
+# dim(temp)
 
 
 is_eigen_zero <- function(M, thresh = 10^-8){
@@ -36,10 +64,9 @@ is_eigen_zero <- function(M, thresh = 10^-8){
   return(any(v < thresh))
 }
 
-
-
 # What will be dimension of reduced demoFram?
 nc = qr(demoFram)$rank
+nc
 
 # Full set of column indices. Will remove those which are linearly dependent
 colIdxSet <- 1:ncol(demoFram)
@@ -47,7 +74,7 @@ colIdxSet <- 1:ncol(demoFram)
 i = 2
 while( i <= length(colIdxSet)){
 
-  print(i)
+  # print(i)
 
   subMat = demoFram[, colIdxSet[1:i]]
 
@@ -62,8 +89,14 @@ while( i <= length(colIdxSet)){
 # Check that my colIdxSet has same length as QR-rank
 length(colIdxSet) == nc
 
+# Removed columns
+(1:ncol(demoFram))[-colIdxSet]
 
 demoFramReduced = demoFram[, colIdxSet]
+
+
+
+
 
 
 
