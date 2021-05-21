16d222b25ae57f845bf688a4aca27673e54e9941
jlivsey <james.a.livsey@gmail.com>
2021-05-21 07:36:22 -0400

    setup IRE scripts

---
+++ tests/sim/20210429-sim/IRE/sim-all/run.R
@@ -0,0 +1,25 @@
+library(Dvar)
+library(L1pack)
+
+#' True table
+load("true-table.RData")
+
+# load marPack object
+load('marPack-all.RData')
+
+# privacy budget parameters
+bpar <- 4
+geoMod <- c(.2, .5, .3)
+queryMod <- c(.1, .2, .5, .2)
+
+# Run simulation
+OUT <-
+  Sim6Way(Nrep    = 10,
+          intab   = A,
+          bpar    = bpar,
+          marPack = marPack,
+          geoMod  = geoMod,
+          queryMod = queryMod)
+
+# Save output
+save(OUT, file = "20210517-results-all.RData")
