2afb0b73f5c781f8a4454aaa9ea4bf048b7de92c
J. Livsey <james.a.livsey@gmail.com>
2021-06-01 08:53:19 -0400

    upd dir

--- tests/sim/20210527-sim/look-at-results.R
+++ tests/sim/20210527-sim/look-at-results.R
@@ -1,31 +1,31 @@
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
 
-simDir <- "tests/sim/20210521-sim/"
-nsim <- 20
+simDir <- "tests/sim/20210527-sim/"
+nsim <- 10
 
 # ---- Load Results ----
 # no margins
-load(file.path(simDir, "20210524-results-non.RData"))
+load(file.path(simDir, "20210527-results-non.RData"))
 non = t(OUT)
 colnames(non) = paste0("non", 1:nsim)
 # tr1 margins (see README)
-load(file.path(simDir, "20210524-results-tr1.RData"))
+load(file.path(simDir, "20210527-results-tr1.RData"))
 tr1 = t(OUT)
 colnames(tr1) = paste0("tr1", 1:nsim)
 # tr2 margins (see README)
-load(file.path(simDir, "20210524-results-tr2.RData"))
+load(file.path(simDir, "20210527-results-tr2.RData"))
 tr2 = t(OUT)
 colnames(tr2) = paste0("tr2", 1:nsim)
 # tr3 margins (see README)
-load(file.path(simDir, "20210524-results-tr3.RData"))
+load(file.path(simDir, "20210527-results-tr3.RData"))
 tr3 = t(OUT)
 colnames(tr3) = paste0("tr3", 1:nsim)
 # cnt margins (see README)
-load(file.path(simDir, "20210524-results-cnt.RData"))
+load(file.path(simDir, "20210527-results-cnt.RData"))
 cnt = t(OUT)
 colnames(cnt) = paste0("cnt", 1:nsim)
 # all margins
-load(file.path(simDir, "20210524-results-all.RData"))
+load(file.path(simDir, "20210527-results-all.RData"))
 all = t(OUT)
 colnames(all) = paste0("all", 1:nsim)
 
@@ -57,7 +57,7 @@ tr3Var = apply(tr3, 1, var)
 cntVar = apply(cnt, 1, var)
 allVar = apply(all, 1, var)
 
-boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar))
+boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar), ylim = c(0, 100))
 
 # ---- What variance is huge? ----
 Av[which(nonVar > 50)]
