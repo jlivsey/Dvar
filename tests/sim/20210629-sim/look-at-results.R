6aef4cdf9117b2150fdf3c6e89ae3819b6d8a9b7
J. Livsey <james.a.livsey@gmail.com>
2021-08-26 08:04:18 -0400

    upd look-at-results.R files for multiple sims

--- tests/sim/20210629-sim/look-at-results.R
+++ tests/sim/20210629-sim/look-at-results.R
@@ -1,31 +1,31 @@
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
 
-simDir <- "tests/sim/20210521-sim/"
-nsim <- 20
+simDir <- "tests/sim/20210623-sim/"
+nsim <- 50
 
 # ---- Load Results ----
 # no margins
-load(file.path(simDir, "20210524-results-non.RData"))
+load(file.path(simDir, "20210623-results-non.RData"))
 non = t(OUT)
 colnames(non) = paste0("non", 1:nsim)
 # tr1 margins (see README)
-load(file.path(simDir, "20210524-results-tr1.RData"))
+load(file.path(simDir, "20210623-results-tr1.RData"))
 tr1 = t(OUT)
 colnames(tr1) = paste0("tr1", 1:nsim)
 # tr2 margins (see README)
-load(file.path(simDir, "20210524-results-tr2.RData"))
+load(file.path(simDir, "20210623-results-tr2.RData"))
 tr2 = t(OUT)
 colnames(tr2) = paste0("tr2", 1:nsim)
 # tr3 margins (see README)
-load(file.path(simDir, "20210524-results-tr3.RData"))
+load(file.path(simDir, "20210623-results-tr3.RData"))
 tr3 = t(OUT)
 colnames(tr3) = paste0("tr3", 1:nsim)
 # cnt margins (see README)
-load(file.path(simDir, "20210524-results-cnt.RData"))
+load(file.path(simDir, "20210623-results-cnt.RData"))
 cnt = t(OUT)
 colnames(cnt) = paste0("cnt", 1:nsim)
 # all margins
-load(file.path(simDir, "20210524-results-all.RData"))
+load(file.path(simDir, "20210623-results-all.RData"))
 all = t(OUT)
 colnames(all) = paste0("all", 1:nsim)
 
@@ -34,20 +34,29 @@ load(file.path(simDir, "true-table.Rdata")) # Loads array A
 Av <- c(A)
 
 # --- Absolue Error ----
-nonErr <- non - Av
-tr1Err <- tr1 - Av
-tr2Err <- tr2 - Av
-tr3Err <- tr3 - Av
-cntErr <- cnt - Av
-allErr <- all - Av
+nonErr <- abs(non - Av)
+tr1Err <- abs(tr1 - Av)
+tr2Err <- abs(tr2 - Av)
+tr3Err <- abs(tr3 - Av)
+cntErr <- abs(cnt - Av)
+allErr <- abs(all - Av)
 
-# ---- Boxplot of absolute error ----
-boxplot(cbind(nonErr[, 1:2],
-              allErr[, 1:2],
-              tr1Err[, 1:2],
-              tr2Err[, 1:2],
-              tr3Err[, 1:2],
-              cntErr[, 1:2]))
+summary(apply(nonErr, 1, mean))
+summary(apply(tr1Err, 1, mean))
+summary(apply(tr2Err, 1, mean))
+summary(apply(tr3Err, 1, mean))
+summary(apply(cntErr, 1, mean))
+summary(apply(allErr, 1, mean))
+
+nonMean <- apply(nonErr, 1, mean)
+tr1Mean <- apply(tr1Err, 1, mean)
+tr2Mean <- apply(tr2Err, 1, mean)
+tr3Mean <- apply(tr3Err, 1, mean)
+cntMean <- apply(cntErr, 1, mean)
+allMean <- apply(allErr, 1, mean)
+
+boxplot(cbind(nonMean, tr1Mean, tr2Mean, tr3Mean, cntMean, allMean),
+        ylim = c(0, 0.015))
 
 # ---- Variance ----
 nonVar = apply(non, 1, var)
@@ -57,10 +66,55 @@ tr3Var = apply(tr3, 1, var)
 cntVar = apply(cnt, 1, var)
 allVar = apply(all, 1, var)
 
-boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar))
+boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar), ylim = c(0, 100))
 
-# ---- What variance is huge? ----
-Av[which(nonVar > 50)]
+# ---- RMSE ----
+# Take mean of all param estimates
+nonMu = apply(non, 1, mean)
+tr1Mu = apply(tr1, 1, mean)
+tr2Mu = apply(tr2, 1, mean)
+tr3Mu = apply(tr3, 1, mean)
+cntMu = apply(cnt, 1, mean)
+allMu = apply(all, 1, mean)
+
+# root mean square error
+nonRMSE = sqrt(apply(non^2, 1, mean) - nonMu^2)
+tr1RMSE = sqrt(apply(tr1^2, 1, mean) - tr1Mu^2)
+tr2RMSE = sqrt(apply(tr2^2, 1, mean) - tr2Mu^2)
+tr3RMSE = sqrt(apply(tr3^2, 1, mean) - tr3Mu^2)
+cntRMSE = sqrt(apply(cnt^2, 1, mean) - cntMu^2)
+allRMSE = sqrt(apply(all^2, 1, mean) - allMu^2)
+
+summary(nonRMSE)
+summary(tr1RMSE)
+summary(tr2RMSE)
+summary(tr3RMSE)
+summary(cntRMSE)
+summary(allRMSE)
+
+# ---- What cell/index has the largest RMSE for each iteration ----
+
+View(nonErr)
+
+table(apply(nonErr, 2, which.max))
+table(apply(tr1Err, 2, which.max))
+table(apply(tr2Err, 2, which.max))
+table(apply(tr3Err, 2, which.max))
+table(apply(cntErr, 2, which.max))
+table(apply(allErr, 2, which.max))
+
+# ---- Lets look at spec index 7220 ----
+idx = 7220
+Av[idx]
+nonErr[idx, ]
+Z = array(0, dim = dim(A)); Z[idx] = 1
+which(Z == 1, arr.ind = TRUE)
+# dim1 = 2 --> Rent
+# dim2 = 2 --> Female
+# dim3 = 6 --> Other race (only variable not at 'highest' value)
+# dim4 = 3 --> age 63+
+# dim5 = 2 --> Non-hisp
+# dim6 = 43 -> tract 43
 
 
 
--- tests/sim/20210701-sim/look-at-results.R
+++ tests/sim/20210701-sim/look-at-results.R
@@ -1,31 +1,51 @@
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
 
-simDir <- "tests/sim/20210521-sim/"
-nsim <- 20
+simDir <- "tests/sim/20210629-sim/"
+nsim <- 50
 
 # ---- Load Results ----
 # no margins
-load(file.path(simDir, "20210524-results-non.RData"))
+load(file.path(simDir, "20210629-results-non.RData"))
 non = t(OUT)
 colnames(non) = paste0("non", 1:nsim)
 # tr1 margins (see README)
-load(file.path(simDir, "20210524-results-tr1.RData"))
+load(file.path(simDir, "20210629-results-tr1.RData"))
 tr1 = t(OUT)
 colnames(tr1) = paste0("tr1", 1:nsim)
 # tr2 margins (see README)
-load(file.path(simDir, "20210524-results-tr2.RData"))
+load(file.path(simDir, "20210629-results-tr2.RData"))
 tr2 = t(OUT)
 colnames(tr2) = paste0("tr2", 1:nsim)
 # tr3 margins (see README)
-load(file.path(simDir, "20210524-results-tr3.RData"))
+load(file.path(simDir, "20210629-results-tr3.RData"))
 tr3 = t(OUT)
 colnames(tr3) = paste0("tr3", 1:nsim)
+# tr4 margins (see README)
+load(file.path(simDir, "20210629-results-tr4.RData"))
+tr4 = t(OUT)
+colnames(tr4) = paste0("tr4", 1:nsim)
+# tr5 margins (see README)
+load(file.path(simDir, "20210629-results-tr5.RData"))
+tr5 = t(OUT)
+colnames(tr5) = paste0("tr5", 1:nsim)
+# tr6 margins (see README)
+load(file.path(simDir, "20210629-results-tr6.RData"))
+tr6 = t(OUT)
+colnames(tr6) = paste0("tr6", 1:nsim)
+# tr7 margins (see README)
+load(file.path(simDir, "20210629-results-tr7.RData"))
+tr7 = t(OUT)
+colnames(tr7) = paste0("tr7", 1:nsim)
+# tr8 margins (see README)
+load(file.path(simDir, "20210629-results-tr8.RData"))
+tr8 = t(OUT)
+colnames(tr8) = paste0("tr8", 1:nsim)
 # cnt margins (see README)
-load(file.path(simDir, "20210524-results-cnt.RData"))
+load(file.path(simDir, "20210629-results-cnt.RData"))
 cnt = t(OUT)
 colnames(cnt) = paste0("cnt", 1:nsim)
 # all margins
-load(file.path(simDir, "20210524-results-all.RData"))
+load(file.path(simDir, "20210629-results-all.RData"))
 all = t(OUT)
 colnames(all) = paste0("all", 1:nsim)
 
@@ -34,30 +54,68 @@ load(file.path(simDir, "true-table.Rdata")) # Loads array A
 Av <- c(A)
 
 # --- Absolue Error ----
-nonErr <- non - Av
-tr1Err <- tr1 - Av
-tr2Err <- tr2 - Av
-tr3Err <- tr3 - Av
-cntErr <- cnt - Av
-allErr <- all - Av
+nonErr <- abs(non - Av)
+tr1Err <- abs(tr1 - Av)
+tr2Err <- abs(tr2 - Av)
+tr3Err <- abs(tr3 - Av)
+tr4Err <- abs(tr4 - Av)
+tr5Err <- abs(tr5 - Av)
+tr6Err <- abs(tr6 - Av)
+tr7Err <- abs(tr7 - Av)
+tr8Err <- abs(tr8 - Av)
+cntErr <- abs(cnt - Av)
+allErr <- abs(all - Av)
 
-# ---- Boxplot of absolute error ----
-boxplot(cbind(nonErr[, 1:2],
-              allErr[, 1:2],
-              tr1Err[, 1:2],
-              tr2Err[, 1:2],
-              tr3Err[, 1:2],
-              cntErr[, 1:2]))
+# ---- Boxplot of absolute error for 1 replicate ----
+boxplot(cbind(nonErr[, 1],
+              tr1Err[, 1],
+              tr2Err[, 1],
+              tr3Err[, 1],
+              tr4Err[, 1],
+              tr5Err[, 1],
+              tr6Err[, 1],
+              tr7Err[, 1],
+              tr8Err[, 1],
+              cntErr[, 1],
+              allErr[, 1]))
+
+# ---- Boxplot of mean absolute error ----
+boxplot(cbind(apply(nonErr, 1, mean),
+              apply(tr1Err, 1, mean),
+              apply(tr2Err, 1, mean),
+              apply(tr3Err, 1, mean),
+              apply(tr4Err, 1, mean),
+              apply(tr5Err, 1, mean),
+              apply(tr6Err, 1, mean),
+              apply(tr7Err, 1, mean),
+              apply(tr8Err, 1, mean),
+              apply(cntErr, 1, mean),
+              apply(allErr, 1, mean)))
 
 # ---- Variance ----
 nonVar = apply(non, 1, var)
 tr1Var = apply(tr1, 1, var)
 tr2Var = apply(tr2, 1, var)
 tr3Var = apply(tr3, 1, var)
+tr4Var = apply(tr4, 1, var)
+tr5Var = apply(tr5, 1, var)
+tr6Var = apply(tr6, 1, var)
+tr7Var = apply(tr7, 1, var)
+tr8Var = apply(tr8, 1, var)
 cntVar = apply(cnt, 1, var)
 allVar = apply(all, 1, var)
 
-boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar))
+# Don't include nonVar for y-scale
+boxplot(cbind(tr1Var,
+              tr2Var,
+              tr3Var,
+              tr4Var,
+              tr5Var,
+              tr6Var,
+              tr7Var,
+              tr8Var,
+              cntVar,
+              allVar), )
 
 # ---- What variance is huge? ----
 Av[which(nonVar > 50)]
