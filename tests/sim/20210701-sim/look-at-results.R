69a231d76e0b206001bae7e0b1edb8358fd74389
jlivsey <james.a.livsey@gmail.com>
2021-08-26 07:44:52 -0400

    20210701-sim results

---
+++ tests/sim/20210701-sim/look-at-results.R
@@ -0,0 +1,66 @@
+library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
+
+simDir <- "tests/sim/20210521-sim/"
+nsim <- 20
+
+# ---- Load Results ----
+# no margins
+load(file.path(simDir, "20210524-results-non.RData"))
+non = t(OUT)
+colnames(non) = paste0("non", 1:nsim)
+# tr1 margins (see README)
+load(file.path(simDir, "20210524-results-tr1.RData"))
+tr1 = t(OUT)
+colnames(tr1) = paste0("tr1", 1:nsim)
+# tr2 margins (see README)
+load(file.path(simDir, "20210524-results-tr2.RData"))
+tr2 = t(OUT)
+colnames(tr2) = paste0("tr2", 1:nsim)
+# tr3 margins (see README)
+load(file.path(simDir, "20210524-results-tr3.RData"))
+tr3 = t(OUT)
+colnames(tr3) = paste0("tr3", 1:nsim)
+# cnt margins (see README)
+load(file.path(simDir, "20210524-results-cnt.RData"))
+cnt = t(OUT)
+colnames(cnt) = paste0("cnt", 1:nsim)
+# all margins
+load(file.path(simDir, "20210524-results-all.RData"))
+all = t(OUT)
+colnames(all) = paste0("all", 1:nsim)
+
+# ---- True Data ----
+load(file.path(simDir, "true-table.Rdata")) # Loads array A
+Av <- c(A)
+
+# --- Absolue Error ----
+nonErr <- non - Av
+tr1Err <- tr1 - Av
+tr2Err <- tr2 - Av
+tr3Err <- tr3 - Av
+cntErr <- cnt - Av
+allErr <- all - Av
+
+# ---- Boxplot of absolute error ----
+boxplot(cbind(nonErr[, 1:2],
+              allErr[, 1:2],
+              tr1Err[, 1:2],
+              tr2Err[, 1:2],
+              tr3Err[, 1:2],
+              cntErr[, 1:2]))
+
+# ---- Variance ----
+nonVar = apply(non, 1, var)
+tr1Var = apply(tr1, 1, var)
+tr2Var = apply(tr2, 1, var)
+tr3Var = apply(tr3, 1, var)
+cntVar = apply(cnt, 1, var)
+allVar = apply(all, 1, var)
+
+boxplot(cbind(nonVar, tr1Var, tr2Var, tr3Var, cntVar, allVar))
+
+# ---- What variance is huge? ----
+Av[which(nonVar > 50)]
+
+
+
---
+++ tests/sim/20210701-sim/run-all.r
@@ -0,0 +1,27 @@
+install.packages("Dvar_0.0.1.tar.gz", repos = NULL, type="source")
+
+library(Dvar)
+library(L1pack)
+
+#' True table
+load("true-table.Rdata")
+
+# load marPack object
+load('marPack-all.RData')
+
+# privacy budget parameters
+eps      <- 5
+geoMod   <- c(1/3, 1/3, 1/3)
+queryMod <- c(1/4, 1/4, 1/4, 1/4)
+
+# Run simulation
+OUT <-
+  Sim6Way(Nrep     = 50,
+          intab    = A,
+          eps      = eps,
+          marPack  = marPack,
+          geoMod   = geoMod,
+          queryMod = queryMod)
+
+# Save output
+save(OUT, file = "20210701-results-all.RData")
