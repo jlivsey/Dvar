db57029728c8558c78262af668ac250e7d4c1b0e
jlivsey <james.a.livsey@gmail.com>
2021-09-10 10:44:49 -0400

    20210909-sim results

---
+++ tests/sim/20210909-sim/look-at-results.R
@@ -0,0 +1,38 @@
+library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
+
+simDir <- "~/Github/Dvar/tests/sim/20210909-sim/run2"
+nsim <- 1
+
+# ---- Load Results ----
+simLevels <- c(0:4, 6:10)
+
+R <- matrix(NA, 7224, length(simLevels))
+colnames(R) <- letters[1:length(simLevels)]
+
+for(jj in seq_along(simLevels)){
+  simNum <- simLevels[jj]
+  print(simNum)
+  fileName <- sprintf("results%d.RData", simNum)
+  print(fileName)
+  load(file.path(simDir, fileName))
+  R[, jj] <- coefEsts
+  colnames(R)[jj] <- paste0("sim", simNum)
+}
+
+head(R)
+
+
+# ---- True Data ----
+load(file.path(simDir, "true-table.Rdata")) # Loads array A
+Av <- c(A)
+
+# ---- Absolue Error ----
+absErr <- abs(R - Av)
+par(mfrow = c(1, 2), mar = c(3, 3, 3, .1))
+boxplot(absErr, main = "absolute error")
+boxplot(absErr, ylim = c(0, 1), main = "zoom in to (0, 1)")
+
+
+
+
+
