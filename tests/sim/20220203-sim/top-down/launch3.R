28e55ea155f79243e506dd1c7fa211459c002f67
jlivsey <james.a.livsey@gmail.com>
2022-02-10 12:30:45 -0500

    20220203-sim results from IRE

---
+++ tests/sim/20220203-sim/top-down/launch3.R
@@ -0,0 +1,12 @@
+load("marPacks-TD.RData")
+idx_sim = 3
+simResultsName = "results3.RData"
+marPack = marPack3
+load("true-table.Rdata")
+
+Nrep = 20
+eps = 10
+geoMod = c(0.4,0.3,0.3)
+queryMod = c(0.2,0.25,0.25,0.3)
+
+source("sim.R")
