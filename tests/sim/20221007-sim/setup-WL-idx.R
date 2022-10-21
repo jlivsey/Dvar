d8faba5a20b294064527cd88374b8c12d3fc260c
James A Livsey (CENSUS/CSRM FED) <james.a.livsey@census.gov>
2022-10-21 15:36:46 +0100

    modify WL sequence to reflect OL doc

--- tests/sim/20221007-sim/setup-WL-idx.R
+++ tests/sim/20221007-sim/setup-WL-idx.R
@@ -1,3 +1,21 @@
+# Setup sequence of workloads for sim study
+
+# Sequence of workloads:
+# 1. Detailed Tract
+# 2. Tract HH/GQ
+# 3. Tract votingAge × hisp × cenRace
+# 4. Tract age × sex
+# 5. County HH/GQ
+# 6. County votingAge × hisp × cenRace
+# 7. County age × sex
+# 8. State HH/GQ
+# 9. State votingAge × hisp × cenRace
+# 10. State age × sex
+# 11. Detailed County
+# 12. Detailed State
+
+# Load WLlist object. List of length 12 with WLlist[[i]] giving row index
+#    of corresponding i(th) WL query.
 load("WLlist.RData")
 
 # ---- WL 1 ----
@@ -18,35 +36,35 @@ WL4idx[WLlist[['trct_agsx']]] <- TRUE
 
 # ---- WL 5 ----
 WL5idx <- WL4idx
-WL5idx[WLlist[['cnty_dtld']]] <- TRUE
+WL5idx[WLlist[['cnty_hhgq']]] <- TRUE
 
 # ---- WL 6 ---
 WL6idx <- WL5idx
-WL6idx[WLlist[['cnty_hhgq']]] <- TRUE
+WL6idx[WLlist[['cnty_vhcr']]] <- TRUE
 
 # ---- WL 7 ---
 WL7idx <- WL6idx
-WL7idx[WLlist[['cnty_vhcr']]] <- TRUE
+WL7idx[WLlist[['cnty_agsx']]] <- TRUE
 
 # ---- WL 8 ---
 WL8idx <- WL7idx
-WL8idx[WLlist[['cnty_agsx']]] <- TRUE
+WL8idx[WLlist[['stat_hhgq']]] <- TRUE
 
 # ---- WL 9 ---
 WL9idx <- WL8idx
-WL9idx[WLlist[['stat_dtld']]] <- TRUE
+WL9idx[WLlist[['stat_vhcr']]] <- TRUE
 
 # ---- WL 10 ---
 WL10idx <- WL9idx
-WL10idx[WLlist[['stat_hhgq']]] <- TRUE
+WL10idx[WLlist[['stat_agsx']]] <- TRUE
 
 # ---- WL 11 ---
 WL11idx <- WL10idx
-WL11idx[WLlist[['stat_vhcr']]] <- TRUE
+WL11idx[WLlist[['cnty_dtld']]] <- TRUE
 
 # ---- WL 12 ---
 WL12idx <- WL11idx
-WL12idx[WLlist[['stat_agsx']]] <- TRUE
+WL12idx[WLlist[['stat_dtld']]] <- TRUE
 
 WLlist_fullLen <- list(
   WL1idx,
